77e5712c78943137406283d6bf5a6d4cf7967a9c
Mohsin Hassan <mhassan94@live.com>
2019-04-27 13:42:54 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express"),
+      app = express(),
+      bodyParser = require('body-parser');
+      indexRoutes = require("./routes/index")
+
+app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(indexRoutes);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
