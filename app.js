a247d5320055f70c6440139b4a018d5f91423325
Mohsin Hassan <mohsin.hassan@factset.com>
2019-04-28 15:22:18 -0400

    Carousel Updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express"),
       indexRoutes = require("./routes/index")
 
 app.use(express.static('public'));
-app.use( express.static( "public" ) );
+app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(indexRoutes);
