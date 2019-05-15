6e03e17aab365b231200c431b6215b04ea3618f1
Mohsin Hassan <mohsin.hassan@factset.com>
2019-05-15 14:01:39 -0400

    added Islamic Finder API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express"),
       app = express(),
-      bodyParser = require('body-parser');
-      indexRoutes = require("./routes/index")
+      bodyParser = require('body-parser'),
+      indexRoutes = require("./routes/index");
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + "/public"))
@@ -11,6 +11,7 @@ app.use(indexRoutes);
 
 //Comment Added for Testing Purpose
 //Only for second branch
+
 
 
 app.listen(3000, function () {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
-var express = require("express")
+var express = require("express"),
+    request = require("request");
 var router = express.Router({mergeParams: true});
 
 router.get("/", function(req, res){
     res.render("index")
+})
+
+router.get("/prayertiming", function(req,res){
+    request("http://www.islamicfinder.us/index.php/api/prayer_times?country=US&zipcode=11235", function(error, response, body){
+            var info = JSON.parse(body);
+            res.send(info["results"]["Fajr"]);
+        
+    })
 })
 
 module.exports = router
