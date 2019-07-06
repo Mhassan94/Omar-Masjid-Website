ffd5c26abd0e30a87a7baa230adcbaf248d2c266
Mohsin Hassan <mohsin.hassan@factset.com>
2019-07-05 21:58:30 -0400

    added nodemailer

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require("express"),
       app = express(),
       bodyParser = require('body-parser'),
       indexRoutes = require("./routes/index");
+      contactUsRoutes = require("./routes/contactus");
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(indexRoutes);
+app.use(contactUsRoutes);
+
 
 //Comment Added for Testing Purpose
 //Only for second branch
-
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
