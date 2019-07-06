6c837bcc5849b92bb6806f2b76aaa4fae1369d9a
Mohsin Hassan <mohsin.hassan@factset.com>
2019-07-05 21:17:49 -0400

    contact us page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get("/", function(req, res){
     res.render("index")
 })
 
+router.get("/contactus", function(req, res){
+    res.render("contactus");
+})
+
 router.get("/prayertiming", function(req,res){
     request("http://www.islamicfinder.us/index.php/api/prayer_times?country=US&zipcode=11235", function(error, response, body){
             var info = JSON.parse(body);
