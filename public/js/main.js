e359a96910aeccb871c547f09ccb81487ea45177
Mohsin Hassan <mohsin.hassan@factset.com>
2019-07-13 22:56:38 -0400

    dfsjalkjflsd

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,10 @@
 jQuery(function($) {'use strict',
-
 	//#main-slider
 	$(function(){
-		$('#main-slider.carousel').carousel({
-			interval: 3000
+		$('#main-slider.item').carousel({
+			interval: 1000
 		});
 	});
-
 
 	// accordian
 	$('.accordion-toggle').on('click', function(){
@@ -19,24 +17,6 @@ jQuery(function($) {'use strict',
 
 	//Initiat WOW JS
 	new WOW().init();
-
-	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
 
 // Contact form
     var form = $('#main-contact-form');
